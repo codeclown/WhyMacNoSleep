25c693097058fac4a4980230a88eed17aa2533ec
Martti Laine <martti@codeclown.net>
2020-02-06 22:12:55 +0100

    Initial Commit

---
+++ MacHasInsomnia/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacHasInsomnia
+//
+//  Created by Martti on 06/02/2020.
+//  Copyright © 2020 Codeclown. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MacHasInsomnia/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacHasInsomnia
+//
+//  Created by Martti on 06/02/2020.
+//  Copyright © 2020 Codeclown. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
